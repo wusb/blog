bb82e7a26b516b8cd41a3eace50bb78d00a23873
simbawu <code@simbawu.com>
2018-02-01 11:10:13 +0800

    add webpack base config

---
+++ config/webpack/webpack.base.js
@@ -0,0 +1,40 @@
+var path = require('path');
+
+module.exports = {
+  entry: {
+    app: ['./src/main.js']
+  },
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: '[name].js',
+    publicPath: "/build/",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|build)/,
+        query: {
+          presets: ['env']
+        }
+      }, {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader",
+          options: {
+            modules: true,
+            localIdentName: '[name]_[local]_[hash:base64:3]'
+          },
+        }, {
+          loader: "sass-loader"
+        }]
+      }
+    ]
+  },
+  devServer: {
+    port: process.env.PORT || 8097
+  }
+};
