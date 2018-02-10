29ce96f8a9d79d6a51add6b8af43c4986d02b4b4
simbawu <code@simbawu.com>
2018-02-10 12:04:12 +0800

    add global css

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import router from './router';
 
 import '../public/assets/css/reset.scss';
+import '../public/assets/css/global.scss';
 
 ReactDOM.render(router,document.getElementById('app'));
