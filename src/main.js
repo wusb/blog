70c150a0278e4f99a38643e9e50da6f1b0231539
simbawu <code@simbawu.com>
2018-02-10 10:12:07 +0800

    add global reset css

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import router from './router';
 
+import '../public/assets/css/reset.scss';
+
 ReactDOM.render(router,document.getElementById('app'));
