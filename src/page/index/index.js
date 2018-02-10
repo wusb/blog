1bff6fb9914a2450ce96c28b7653726250de62b9
simbawu <code@simbawu.com>
2018-02-10 12:04:27 +0800

    add index page

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -5,13 +5,34 @@ import { Link } from 'react-router-dom';
 class IndexPage extends React.Component {
 	constructor(){
 		super();
+		this.state = {
+
+		}
 	}
+
+	componentDidMount(){
+
+	}
+
 
 	render() {
 		return (
 			<div className={s.container}>
-				Hello World
-				<Link to='/blog'>blog</Link>
+				<div className={s.photo}></div>
+				<div className={s.introduce}>
+					<p>Hi Friend,</p>
+					<div>
+            I’m Simba Wu.<br/> a Front End Developer in Shanghai.
+					</div>
+				</div>
+				<div className={s.link}>
+          <a href="https://github.com/simbawus">
+						<i className='iconfont'>&#xe600;</i>
+					</a>
+          <Link to='/blog'>
+						<i className='iconfont'>&#xe70f;</i>
+					</Link>
+				</div>
 			</div>
 		);
 	}
