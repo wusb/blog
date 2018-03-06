7ab6cd960e3cf8b5972d48dacf2f058ff6996e95
simbawu <code@simbawu.com>
2018-03-06 10:26:25 +0800

    博客列表增加滑动切换动效

--- src/page/blog/index.js
+++ src/page/blog/index.js
@@ -11,10 +11,27 @@ class IndexPage extends React.Component {
     this.state = {
       labels: [],
       list: [],
-      currentLabel: 'New'
+      currentLabel: 'New',
+      opacity: 1
+    };
+
+    this.localState = {
+      startX: 0,
+      startY: 0,
+      startT: 0,
+      isMove: false,
+      isTouchEnd: true,
+      initTab: 0,
+      currentTab: 0,
+      direction: 'left'
     };
 
     this.chooseLabel = this.chooseLabel.bind(this);
+    this.touchStart = this.touchStart.bind(this);
+    this.touchMove = this.touchMove.bind(this);
+    this.touchEnd = this.touchEnd.bind(this);
+    this.handleLabelIndex = this.handleLabelIndex.bind(this);
+    this.handleOpacity = this.handleOpacity.bind(this);
   }
 
   componentDidMount(){
@@ -25,6 +42,7 @@ class IndexPage extends React.Component {
 
   getIssues(label = 'New', page = null){
     let data = {};
+    this.localState.initLabel = label;
     if(label == 'New' || !this.state.labels.includes(label)){
       label = 'New';
       data = {
@@ -89,7 +107,8 @@ class IndexPage extends React.Component {
       this.setState({
         labels: labels,
         list: list,
-        currentLabel: label
+        currentLabel: label,
+        opacity: 1
       })
 
     })
@@ -116,23 +135,90 @@ class IndexPage extends React.Component {
   }
 
   chooseLabel(currentLabel){
+    this.handleOpacity(0);
     this.props.history.push(`/blog/${currentLabel}`);
-
     this.getIssues(currentLabel);
+  }
 
+  touchStart(e){
+    // e.preventDefault();
+    if(e.touches.length == 1 || this.localState.isTouchEnd){
+      let touch = e.touches[0];
+      this.localState.startX = touch.pageX;
+      this.localState.startY = touch.pageY;
+      this.localState.initTab = this.state.currentTab;   //本次滑动前的初始位置
+      this.localState.startT = new Date().getTime(); //记录手指按下的开始时间
+      this.localState.isMove = false; //是否产生滑动
+      this.localState.isTouchEnd = false; //当前开始滑动
+    }
+  }
+
+  touchMove(e){
+    // e.preventDefault();
+    let touch = e.touches[0];
+    let deltaX = touch.pageX - this.localState.startX;
+    let deltaY = touch.pageY - this.localState.startY;
+    //如果X方向上的位移大于Y方向，则认为是左右滑动
+    if (Math.abs(deltaX) > Math.abs(deltaY)){
+      if ((this.handleLabelIndex(this.state.currentLabel) > 0 && deltaX > 0) || (this.handleLabelIndex(this.state.currentLabel) <= (this.state.labels.length - 2) && deltaX < 0)){
+        //移动页面
+        this.handleOpacity(0);
+        this.localState.isMove = true;
+      }
+      this.localState.direction = deltaX > 0 ? "right" : "left"; //判断手指滑动的方向
+    }
+  }
+
+  touchEnd(e){
+    // e.preventDefault();
+    let translate = 0;
+    //计算手指在屏幕上停留的时间
+    let deltaT = new Date().getTime() - this.localState.startT;
+    let index = 0;
+
+    console.log(deltaT);
+    if (this.localState.isMove){ //发生了左右滑动
+      //使用动画过渡让页面滑动到最终的位置
+      if(deltaT < 300){ //如果停留时间小于300ms,则认为是快速滑动，无论滑动距离是多少，都停留到下一页
+        index = this.localState.direction == 'left'? 1 : -1;
+
+      }else {
+        //如果滑动距离小于屏幕的50%，则退回到上一页
+        if (Math.abs(this.localState.moveLength)/this.localState.pageWidth < 0.5){
+          translate = this.localState.currentPosition-this.localState.moveLength;
+
+          index = 0
+
+        }else{
+          //如果滑动距离大于屏幕的50%，则滑动到下一页
+          index = this.localState.direction == 'left'? 1 : -1;
+        }
+      }
+
+      console.log(this.state.labels.indexOf(this.state.currentLabel));
+
+      this.chooseLabel(this.state.labels[this.state.labels.indexOf(this.state.currentLabel) + index]);
+
+    }
+  }
+
+  handleOpacity(opacity){
     this.setState({
-      currentLabel: currentLabel
-    });
+      opacity: opacity
+    })
+  }
 
+  handleLabelIndex(label){
+    return this.state.labels.indexOf(label)
   }
 
   render() {
     return (
-        <div className={s.container}>
+        <div className={s.container} onTouchStart={this.touchStart} onTouchMove={this.touchMove} onTouchEnd={this.touchEnd}>
           <ul className={s.labels}>
             {this._renderLabels()}
           </ul>
-          <div className={s.list}>
+          <div className={s.list} style={{opacity: this.state.opacity}} >
             {this._renderList()}
           </div>
         </div>
