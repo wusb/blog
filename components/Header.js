b7d0bd0cdd53dab28a94de35df730c1f168372a2
simbawu <code@simbawu.com>
2018-03-26 00:12:58 +0800

    Header component add search call

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './Header.scss';
-import { Link, browserHistory } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 class Header extends React.Component {
   constructor(){
@@ -13,11 +13,14 @@ class Header extends React.Component {
         name: 'Blog',
         url: '/blog'
       }],
+      keyword: '',
       searchBoxFull: false
     };
 
     this._renderMenus = this._renderMenus.bind(this);
     this.handleSearchBox = this.handleSearchBox.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleInput = this.handleInput.bind(this);
   }
 
   componentDidMount(){
@@ -39,8 +42,17 @@ class Header extends React.Component {
     })
   }
 
+  handleInput(e){
+    let keyword = e.target.value;
+    this.setState({
+      keyword: keyword
+    })
+  }
+
   handleSearch(e){
     e.preventDefault();
+    let keyWords = this.state.keyword;
+    this.props.history.push(`/search?keyWords=${keyWords}`);
   }
 
   render() {
@@ -51,7 +63,7 @@ class Header extends React.Component {
               {this._renderMenus()}
             </div>
             <form className={s.searchBox} onSubmit={this.handleSearch}>
-              <input type="srarch" placeholder='搜索' onFocus={() => this.handleSearchBox(true)} onBlur={() => this.handleSearchBox(false)}/>
+              <input type="srarch" placeholder='搜索' onChange={this.handleInput} onFocus={() => this.handleSearchBox(true)} onBlur={() => this.handleSearchBox(false)}/>
               <i className='iconfont' onClick={this.handleSearch}>&#xe605;</i>
             </form>
           </div>
@@ -60,4 +72,4 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+export default withRouter(Header);
